04643b1ba211118804746d3ded9068879d76ef20
Scott Bowler <scott.bowler@learningfuze.com>
2019-05-13 18:26:41 -0700

    Fixed order of error keys

--- middleware/error_handling.js
+++ middleware/error_handling.js
@@ -2,9 +2,9 @@ const { formatError } = require(__basedir + '/helpers/error_handling');
 
 module.exports = (err, req, res, next) => {
     const error = {
+        errors: formatError(err, 'An error occurred'),
         message: `Bad ${req.method} Request`,
-        success: false,
-        errors: formatError(err, 'An error occurred')
+        success: false
     };
     const status = err.status || 500;
 
