1e402255f38e6f8bd6d2650a0cb394fc02f555bf
Scott Bowler <scott.bowler@learningfuze.com>
2019-05-18 11:06:59 -0700

    Removed unused files

--- controllers/api/orders/index.js
+++ controllers/api/orders/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     getAllUserOrders: require('./get_all_user_orders'),
-    getGuestOrderDetails: require('./get_guest_order_details'),
     getOrderDetails: require('./get_order_details'),
     newGuestOrder: require('./create_guest_order'),
     newUserOrder: require('./create_user_order')
